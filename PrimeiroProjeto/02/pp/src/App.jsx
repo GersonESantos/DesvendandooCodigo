e6c19b64af15908a706c8c6995093f1551b3b484
GersonESS <gebhsantos@gmail.com>
2025-11-10 20:31:12 -0300

    02

---
+++ PrimeiroProjeto/02/pp/src/App.jsx
@@ -0,0 +1,25 @@
+function Cabecalho() {
+  return (
+    <header style={{backgroundColor:'#20232a', color:'white', padding:'10px'}}>
+      <h1>Meu Primeiro Projeto React Exemplo1(JSX)</h1>
+      <p>Usando Vite  </p>
+
+    </header>
+  );
+}
+
+export default function App() {
+  
+  return (
+    <div> 
+      <Cabecalho/>
+      <footer style={{textAlign: 'center', padding: '20px', backgroundColor:'#eee', marginTop:'20px'}}>
+        <p>Rodapé do meu primeiro projeto React</p>
+      </footer> 
+
+    </div>
+  );
+}
+
+
+// export default Cabecalho; 
