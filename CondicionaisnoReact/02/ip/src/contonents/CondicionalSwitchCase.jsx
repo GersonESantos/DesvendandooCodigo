d163c1cf93a05ae5f6f4197f259ef05018613074
GersonESS <gebhsantos@gmail.com>
2025-11-15 12:35:06 -0300

    Update CondicionalSwitchCase.jsx

--- CondicionaisnoReact/02/ip/src/contonents/CondicionalSwitchCase.jsx
+++ CondicionaisnoReact/02/ip/src/contonents/CondicionalSwitchCase.jsx
@@ -1,24 +1,35 @@
 import { useState } from "react";
-function CondicionalSwitch() {
-    const [dia, setDia] = useState("segunda"); // 0-Domingo, 1-Segunda, 2-Terça, 3-Quarta, 4-Quinta, 5-Sexta, 6-Sábado 
-    let nomeDia;
-    const mensagem = (()=>{
-        switch (dia) {
-        case "Segunda":
-            return "inicio da semana vamos la "
-        case "Sexta":
-            return "Sextou !!!!!!! "    
-        
-        default:
-            nomeDia = "Dia inválido";
-    }
-     })
-       
+
+function CondicionalSwitchCase() {
+    const [dia, setDia] = useState("Segunda");
+
+    const mensagem = 
+    ( () => 
+      {
+        switch (dia) 
+        {
+            case "Segunda":
+                return "inicio da semana vamos la !!!!!!! 😊 ";
+            case "Sexta":
+                return "Sextou !!!!!!! ";
+            case "Domingo":
+                return "Domingou, dia de descanso !";
+            default:
+                return "Dia de estudo no React !!!!!!!";
+        }
+     }
+    )(); // <-- invoca a função imediatamente para obter a string
+
     return (
         <div>
-            <h3>Condicional com Switch</h3>
-            <p>Hoje é: {nomeDia}</p>
+            <h3>Condicional com Switch case </h3>
+            <p>Hoje é: <strong>{dia}</strong></p>
+            <p>{mensagem}</p>
+            <button onClick={() => setDia("Segunda")}>Segunda</button>
+            <button onClick={() => setDia("Sexta")}>Sexta</button>
+            <button onClick={() => setDia("Domingo")}>Domingo</button>
         </div>
-    )
+    );
 }
+
 export default CondicionalSwitchCase;
