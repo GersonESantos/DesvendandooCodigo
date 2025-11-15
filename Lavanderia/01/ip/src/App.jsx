9bd9b7072fd2611e87f7db41725558e7f7bb98b8
Gerson Eustáquio dos Santos <gebhsantos@gmail.com>
2025-11-15 19:35:56 -0300

    a

---
+++ Lavanderia/01/ip/src/App.jsx
@@ -0,0 +1,14 @@
+import CondicionalSimples from "./contonents/condicionalSimples"
+import Separador from "./contonents/Separador"
+
+function App() {  
+  return (
+    <div style={{padding:"20px"}}>
+      <h2>Exemplos de Condicionais no React</h2>
+      <CondicionalSimples />
+      <Separador />
+    </div>
+  )
+}
+
+export default App
